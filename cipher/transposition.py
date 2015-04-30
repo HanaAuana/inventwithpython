f86c0fceb6e3a004f7c8554f0c2b09807ff5f57a
Michael Lim <hanaauana@gmail.com>
2015-04-29 20:51:46 -0700

    Decoding transposition cipher. Practice using math.ceil()

--- cipher/transposition.py
+++ cipher/transposition.py
@@ -1,3 +1,4 @@
+import math
 
 
 def encode_transposition(text, key):
@@ -30,6 +31,32 @@ def encode_transposition(text, key):
         encoded.append(''.join(col))
     return ''.join(encoded)
 
+
+def decode_transposition(text, key):
+    num_cols = math.ceil(len(text) / key)
+    num_rows = key
+    # Record the cells we won't need in the "table"
+    num_unused = (num_cols * num_rows) - len(text)
+
+    decoded_cols = [[] for r in range(num_cols)]
+    col = 0
+    row = 0
+    for c in text:
+        decoded_cols[col].append(c)
+        col += 1
+
+        last_col = col == num_cols
+        in_unused = (col == (num_cols - 1) and row >= num_rows - num_unused)
+        # If we reach the last column, or if we're in an unused cell
+        if last_col or in_unused:
+            # Move to the next row
+            col = 0
+            row += 1
+
+    decoded = [''.join(c) for c in decoded_cols]
+    return ''.join(decoded)
+
+
 # to_encode = input('Enter some text: ').strip()
 # key = int(input('Enter your caesar shift key: '))
 to_encode = "Common sense is not so common."
@@ -37,9 +64,9 @@ key = 8
 encoded = encode_transposition(to_encode, key)
 print('Encoded: ')
 print(encoded)
-# print('Decoded: ')
+print('Decoded: ')
 # 'Encode' the decoded string using additive inverse or encoding key to decode
-# print(encode_transposition(encoded, 0 - key))
+print(decode_transposition(encoded, key))
 
 
 def encode_transposition_comp(text, key):
@@ -53,4 +80,4 @@ def encode_transposition_comp(text, key):
     # Then join each column back into a single string
     return(''.join(encoded))
 
-print(encode_transposition_comp(to_encode, key))
+# print(encode_transposition_comp(to_encode, key))
